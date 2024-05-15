3d74e88dfc093b5fdb9584a3c3b968b273e0afee
Johannes Braun <Braun-Johannes@outlook.de>
2024-05-15 10:29:50 +0200

    all

--- Backend/backend-basics_chance/server.js
+++ Backend/backend-basics_chance/server.js
@@ -8,6 +8,7 @@ const profession = chance.profession();
 
 export const server = createServer((req, res) => {
   res.statusCode = 200;
+
   res.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
   );
