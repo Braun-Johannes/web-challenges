ac0ef0f70cc7c4be0836a206674014a569799e6d
Johannes Braun <Braun-Johannes@outlook.de>
2024-05-17 10:52:11 +0200

    added new form to fishshop

--- Backend/backend-read_products/pages/api/products/index.js
+++ Backend/backend-read_products/pages/api/products/index.js
@@ -8,4 +8,16 @@ export default async function handler(request, response) {
     const fish = await Product.find();
     return response.status(200).json(fish);
   }
+
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+
+      response.status(201).json({ status: "Fish added" });
+    } catch (error) {
+      console.error(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 }
